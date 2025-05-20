63186fc392926e9c401e805dd2fe31225da5d2b2
bryn lloyd <bryn.lloyd@jesus.ox.ac.uk>
2025-05-20 11:15:58 +0100

    added additional dopant materials: Plutonium and Californium

--- src/brynDetectorConstruction.cc
+++ src/brynDetectorConstruction.cc
@@ -35,11 +35,45 @@ G4VPhysicalVolume* brynDetectorConstruction::Construct() {
     a = 58.7 * g/mole;
     G4Element* Ni = new G4Element("Nickel", "Ni", z, a);
 
+    // Uranium
+    z = 92.;
+    a = 238. * g/mole;
+    G4Element* U = new G4Element("Uranium", "U", z, a);
+
+    // Plutonium
+    z = 94.;
+    a = 244. * g/mole;
+    G4Element* Pu = new G4Element("Plutonium", "Pu", z, a);
+
+
+    // Californium
+    z = 98.;
+    a = 251. * g/mole;
+    G4Element* Ca = new G4Element("Californium", "Ca", z, a);
+
     // meteorite material is 85% iron, 15% nickel
     G4Material* meteoriteMaterial = new G4Material("meteoriteMaterial", 8.02 * g/cm3, 2);
     meteoriteMaterial->AddElement(Fe, 0.85);
     meteoriteMaterial->AddElement(Ni, 0.15);
 
+    // meteorite material that keeps Fe:Ni ratio, but adds in uranium as a dopant to see its effect on the hadron shower
+    // apply correction to the density??
+    G4Material*dopedMeteoriteMaterial = new G4Material("dopedMeteoriteMaterial", 8.02 * g/cm3, 3);
+    G4double ironPercentage = 0.85;
+    G4double nickelPercentage = 0.15;
+    G4double uraniumPercentage = 0.1;
+    G4double total = ironPercentage + nickelPercentage + uraniumPercentage;
+    G4double ironPercentageCorrected = ironPercentage / total;
+    G4double nickelPercentageCorrected = nickelPercentage / total;
+    G4double uraniumPercentageCorrected = uraniumPercentage / total;
+
+    // std::cout << "N's" << ironPercentageCorrected << " " << nickelPercentageCorrected << " " << uraniumPercentageCorrected << std::endl;
+
+
+
+    dopedMeteoriteMaterial->AddElement(Fe, ironPercentageCorrected);
+    dopedMeteoriteMaterial->AddElement(Ni, nickelPercentageCorrected);
+    dopedMeteoriteMaterial->AddElement(U, uraniumPercentageCorrected);
 
     ///////////////////////
     // setup of geometry //
@@ -53,7 +87,7 @@ G4VPhysicalVolume* brynDetectorConstruction::Construct() {
 
     // meteorite
     G4VSolid* solidMeteorite = new G4Tubs("solidMeteorite", 0.*m, 0.005*m, 0.05*m, 0., 360.0*deg);
-    G4LogicalVolume* logicMeteorite = new G4LogicalVolume(solidMeteorite, meteoriteMaterial, "logicMeteorite");
+    G4LogicalVolume* logicMeteorite = new G4LogicalVolume(solidMeteorite, dopedMeteoriteMaterial, "logicMeteorite");
     G4VPhysicalVolume* physMeteorite = new G4PVPlacement(0, G4ThreeVector(), logicMeteorite, "logicMeteorite", logicWorld, false, 0, checkOverlaps);
 
     //detector
